2f77a668a87ee09b89115df068c95633995f2451
Navneet Shahi <navneetshahi345@gmail.com>
2024-11-11 19:45:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold text-red-900">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
